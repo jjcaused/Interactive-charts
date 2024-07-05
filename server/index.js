4ab3bb694e357d46f17a07c6032462b5a42d936d
jjcaused <jayanth8088@gmail.com>
2024-07-05 13:25:53 +0530

    frontend changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://interactive-charts-frontend.vercel.app/"],
+    origin: ["https://interactive-charts-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
@@ -14,7 +14,8 @@ app.use(
 app.use(express.json());
 
 mongoose.connect(
-  "mongodb+srv://jayanth8088:NvPXNJuYjbwu0f6x@cluster0.ceofnct.mongodb.net/Importing_DB?retryWrites=true&w=majority&appName=Cluster0/"
+  "mongodb+srv://jayanth8088:NvPXNJuYjbwu0f6x@cluster0.ceofnct.mongodb.net/Importing_DB?retryWrites=true&w=majority&appName=Cluster0/",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 app.get("/", async (req, res) => {
@@ -22,11 +23,14 @@ app.get("/", async (req, res) => {
     const users = await usermodel.find({}).limit(100);
     console.log(users);
     res.json(users);
-   
   } catch (err) {
     console.error("Error fetching data:", err);
     res.status(500).json({ message: "Internal server error" });
   }
+});
+
+app.listen(3001, () => {
+  console.log("server is running on port 3001");
 });
 
 // app.post("/getDataNew", async (req, res) => {
@@ -38,7 +42,3 @@ app.get("/", async (req, res) => {
 //     console.error("Error fetching data:", err);
 //   }
 // });
-
-app.listen(3001, () => {
-  console.log("server is running");
-});
